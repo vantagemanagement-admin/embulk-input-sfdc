76cf0f8c6b9b45318b98af467855fe91fb2b6e21
yoshihara <h.yoshihara@everyleaf.com>
2015-06-02 18:28:16 +0900

    test: add the test for Sfdc::Api.setup

--- test/sfdc/test-api.rb
+++ test/sfdc/test-api.rb
@@ -1,6 +1,17 @@
 require "sfdc/api"
 
 class SfdcApiTest < Test::Unit::TestCase
+  def test_setup
+    config = {}
+
+    any_instance_of(Sfdc::Api) do |klass|
+      mock(klass).authentication(config) { "access_token" }
+      mock(klass).set_latest_version("access_token") { klass }
+    end
+
+    Sfdc::Api.setup("login_url", config)
+  end
+
   def test_initialize
     api = Sfdc::Api.new("login_url")
 
