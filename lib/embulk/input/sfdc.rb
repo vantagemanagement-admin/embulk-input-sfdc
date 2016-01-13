cb2c191171492113ee5dafac43461bb39627068d
uu59 <k@uu59.org>
2016-01-13 17:05:40 +0900

    Add disabled_config_diff_merge for resume strategy handling

--- lib/embulk/input/sfdc.rb
+++ lib/embulk/input/sfdc.rb
@@ -24,6 +24,7 @@ module Embulk
         task[:continue_from] = config.param("continue_from", :string, default: nil)
 
         task[:schema] = config.param("columns", :array)
+        task[:disabled_config_diff_merge] = config.param("disabled_config_diff_merge", :boolean, default: true)
         columns = []
 
         task[:schema].each do |column|
@@ -90,6 +91,8 @@ module Embulk
         page_builder.finish
 
         Embulk.logger.debug "Added all records."
+
+        return {} if task[:disabled_config_diff_merge]
 
         task_report = {
           continue_from: @latest_updated.to_s
